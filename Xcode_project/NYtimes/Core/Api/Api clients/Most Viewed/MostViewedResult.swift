cfc35647b465b7295bb00d7ca1ab53b89a567a5f
Michał Mańkus <mich.mankus@gmail.com>
2019-02-18 21:31:56 +0100

    Fixed parsing data fail

--- Xcode_project/NYtimes/Core/Api/Api clients/Most Viewed/MostViewedResult.swift
+++ Xcode_project/NYtimes/Core/Api/Api clients/Most Viewed/MostViewedResult.swift
@@ -10,7 +10,7 @@ import Foundation
 
 struct MediaMetadata: Codable {
     let url: URL
-    let format: String
+    let format: String?
     let height: Int
     let width: Int
 }
@@ -26,19 +26,19 @@ struct ArticleMedia: Codable {
         case mediaMetadata = "media-metadata"
     }
     
-    let type: String
-    let subtype: String
-    let caption: String
-    let copyright: String
+    let type: String?
+    let subtype: String?
+    let caption: String?
+    let copyright: String?
     let approved_for_syndication: Int
     let mediaMetadata: [MediaMetadata]
     
     init(from decoder: Decoder) throws {
         let values = try decoder.container(keyedBy: CodingKeys.self)
-        type = try values.decode(String.self, forKey: .type)
-        subtype = try values.decode(String.self, forKey: .subtype)
-        caption = try values.decode(String.self, forKey: .caption)
-        copyright = try values.decode(String.self, forKey: .copyright)
+        type = try values.decodeIfPresent(String.self, forKey: .type)
+        subtype = try values.decodeIfPresent(String.self, forKey: .subtype)
+        caption = try values.decodeIfPresent(String.self, forKey: .caption)
+        copyright = try values.decodeIfPresent(String.self, forKey: .copyright)
         approved_for_syndication = try values.decode(Int.self, forKey: .approved_for_syndication)
         mediaMetadata = try values.decode([MediaMetadata].self, forKey: .mediaMetadata)
     }
